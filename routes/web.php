2a25387b26bb44f9c19673c2b751359b01af3d73
egiputra <egiputra450@gmail.com>
2025-01-02 21:08:16 +0700

    add route to routing shorten url

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UrlController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/{short_url}', [UrlController::class, 'index']);
 
 require __DIR__.'/auth.php';
